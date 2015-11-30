a83a5bdb622acb13b93e5a7d8cd6a86c687c7c2d
Albert Sanchez <albert.san.iza@gmail.com>
2015-11-30 10:53:50 -0800

    Added a cube
    from the getting started of three js

--- www/js/directives.js
+++ www/js/directives.js
@@ -8,7 +8,86 @@ angular.module('starter.directives', [])
   };
 
   function link($scope, $element, $attr) {
+    var scene, camera, renderer, element, container, effect, controls, clock;
+    var cube;
+    init();
+    function init() {
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.001, 2100);
+      camera.position.set(0, 10, 0);
+      scene.add(camera);
+      renderer = new THREE.WebGLRenderer({
+        antialias: true
+      });
+      element = renderer.domElement;
+      container = $element[0];
+      container.appendChild(element);
+      effect = new THREE.StereoEffect(renderer);
+      // Controls
+      controls = new THREE.OrbitControls(camera, element);
+      controls.target.set(camera.position.x + 0.15, camera.position.y, camera.position.z);
+      controls.noPan = true;
+      controls.noZoom = true;
+      function setOrientationControls(e) {
+        if (!e.alpha) {
+          return;
+        }
+        controls = new THREE.DeviceOrientationControls(camera, true);
+        controls.connect();
+        controls.update();
+        element.addEventListener('click', fullscreen, false);
+        window.removeEventListener('deviceorientation', setOrientationControls, true);
+      };
+      window.addEventListener('deviceorientation', setOrientationControls, true);
+      // Lighting
+      light = new THREE.AmbientLight(0xd9d9d9);
+      scene.add(light);
 
+      var geometry = new THREE.BoxGeometry( 2, 2, 2 );
+      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+      cube = new THREE.Mesh( geometry, material );
+      scene.add( cube );
+
+      clock = new THREE.Clock();
+      animate();
+    };
+    function animate() {
+      var elapsedSeconds = clock.getElapsedTime();
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.1;
+      cube.rotation.y += 0.1;
+
+      update(clock.getDelta());
+      render(clock.getDelta());
+    };
+    function update(dt) {
+      resize();
+      camera.updateProjectionMatrix();
+      controls.update(dt);
+    };
+    function render() {
+      renderer.render(scene, camera);
+      //effect.render(scene, camera);
+    };
+    function resize() {
+      var width = container.offsetWidth;
+      var height = container.offsetHeight;
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderer.setSize(width, height);
+      effect.setSize(width, height);
+    };
+    function fullscreen() {
+      if (container.requestFullscreen) {
+        container.requestFullscreen();
+      } else if (container.msRequestFullscreen) {
+        container.msRequestFullscreen();
+      } else if (container.mozRequestFullScreen) {
+        container.mozRequestFullScreen();
+      } else if (container.webkitRequestFullscreen) {
+        container.webkitRequestFullscreen();
+      }
+    };
   };
-
 }])
