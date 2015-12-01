84adff7d539dfc453f058e22d9a43ec2e4e41d6a
Albert Sanchez <albert.san.iza@gmail.com>
2015-11-30 16:19:21 -0800

    Cardboard switch option

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,21 @@
 angular.module('starter.controllers', [])
 
-.controller('MainCtrl', function($scope) {
-  
+.controller('MainCtrl', function($scope, $ionicModal) {
+  $scope.Data = {
+    StereoEffect: false
+  };
+
+  $ionicModal.fromTemplateUrl('templates/settings.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.settingsModal = modal;
+  });
+  $scope.openSettingsModal = function() {
+    $scope.settingsModal.show();
+  };
+  $scope.closeSettingsModal = function() {
+    $scope.settingsModal.hide();
+  };
+
 })
