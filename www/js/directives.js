21d0055f7a4ad8e385afc517b1e3961406c4ba9a
Albert Sanchez <albert.san.iza@gmail.com>
2015-11-30 16:09:16 -0800

    Removed threex
    Planets are now loaded directly in mi init because threex is using
    deprecated functions.

--- www/js/directives.js
+++ www/js/directives.js
@@ -13,25 +13,13 @@ angular.module('starter.directives', [])
   function link($scope, $element, $attr) {
     var scene, camera, renderer, element, container, effect, controls, clock;
     var Starfield, Sun, Mercury, Venus, Earth, EarthMoon, Mars, Jupiter, Saturn, SaturnRing, Uranus, Neptune, Pluto;
-    var BaseSize = 100;
-    var StarfieldSize = 50000;
-    var SunSize = 2000;
-    var MercurySize = BaseSize * 0.382;
-    var VenusSize = BaseSize * 0.949;
-    var EarthSize = BaseSize;
-    var MarsSize = BaseSize * 0.532;
-    var JupiterSize = BaseSize * 11.209;
-    var SaturnSize = BaseSize * 9.44;
-    var UranusSize = BaseSize * 4.007;
-    var NeptuneSize = BaseSize * 3.883;
-    var PlutoSize = BaseSize * 0.180;
-    var EarthBaseDistance = 1000;
+    var StarfieldSize = 20000;
     init();
     function init() {
       // Main Scene
       scene = new THREE.Scene();
       camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.001, StarfieldSize + 100);
-      camera.position.set(0, 3000, 5000);
+      camera.position.set(0, 500, 1000);
       scene.add(camera);
       renderer = new THREE.WebGLRenderer({antialias: true});
       element = renderer.domElement;
@@ -68,76 +56,117 @@ angular.module('starter.directives', [])
       var Loader_Texture = new THREE.TextureLoader(manager);
       var Loader_OBJ = new THREE.OBJLoader(manager);
       // Textures
-      Loader_Texture.load('lib/threex/threex.planets/images/galaxy_starfield.png', function(texture) {
+      Loader_Texture.load('img/planets/galaxy_starfield.png', function(texture) {
         var Material = new THREE.MeshPhongMaterial({map: texture, side: THREE.BackSide});
-        var Geometry = new THREE.SphereGeometry(100000, 32, 32);
-        var Starfield = new THREE.Mesh(Geometry, Material);
+        var Geometry = new THREE.SphereGeometry(StarfieldSize, 32, 32);
+        Starfield = new THREE.Mesh(Geometry, Material);
         scene.add(Starfield);
       });
-      Sun = THREEx.Planets.createSun();
-      Sun.geometry = new THREE.SphereGeometry(SunSize, 32, 32);
-      scene.add(Sun);
-      Mercury = THREEx.Planets.createMercury();
-      Mercury.geometry = new THREE.SphereGeometry(MercurySize, 32, 32);
-      Mercury.position.set(0, 0, SunSize + (EarthBaseDistance * 0.39));
-      scene.add(Mercury);
-      Venus = THREEx.Planets.createVenus();
-      Venus.geometry = new THREE.SphereGeometry(VenusSize, 32, 32);
-      Venus.position.set(0, 0, SunSize + (EarthBaseDistance * 0.72));
-      scene.add(Venus);
-      Earth = THREEx.Planets.createEarth();
-      Earth.geometry = new THREE.SphereGeometry(EarthSize, 32, 32);
-      Earth.position.set(0, 0, SunSize + EarthBaseDistance);
-      scene.add(Earth);
-      EarthMoon = THREEx.Planets.createMoon();
-      EarthMoon.geometry = new THREE.SphereGeometry(10, 32, 32);
-      scene.add(EarthMoon);
-      Mars = THREEx.Planets.createMars();
-      Mars.geometry = new THREE.SphereGeometry(MarsSize, 32, 32);
-      Mars.position.set(0, 0, SunSize + (EarthBaseDistance * 1.52));
-      scene.add(Mars);
-      Jupiter = THREEx.Planets.createJupiter();
-      Jupiter.geometry = new THREE.SphereGeometry(JupiterSize, 32, 32);
-      Jupiter.position.set(0, 0, SunSize + (EarthBaseDistance * 4.20));
-      scene.add(Jupiter);
-      Saturn = THREEx.Planets.createSaturn();
-      Saturn.geometry = new THREE.SphereGeometry(SaturnSize, 32, 32);
-      Saturn.position.set(0, 0, SunSize + (EarthBaseDistance * 7.54));
-      scene.add(Saturn);
-      SaturnRing = new THREE.Mesh(new THREE.XRingGeometry(SaturnSize, (SaturnSize) * 1.5, 50, 6, 0, Math.PI * 2), new THREE.MeshBasicMaterial({
-        map: THREE.ImageUtils.loadTexture('lib/threex/threex.planets/images/saturn-rings.png'),
-        side: THREE.DoubleSide,
-        transparent: true,
-        opacity: 0.6
-      }));
-      SaturnRing.position.set(0, 0, SunSize + (EarthBaseDistance * 7.54));
-      scene.add(SaturnRing);
-      Uranus = THREEx.Planets.createUranus();
-      Uranus.geometry = new THREE.SphereGeometry(UranusSize, 32, 32);
-      Uranus.position.set(0, 0, SunSize + (EarthBaseDistance * 16.18));
-      scene.add(Uranus);
-      /*
-      UranusRing = THREEx.Planets.createUranusRing();
-      UranusRing.geometry = new THREE.SphereGeometry(12, 32, 32);
-      scene.add(UranusRing);
-      */
-      Neptune = THREEx.Planets.createNeptune();
-      Neptune.geometry = new THREE.SphereGeometry(NeptuneSize, 32, 32);
-      Neptune.position.set(0, 0, SunSize + (EarthBaseDistance * 25.06));
-      scene.add(Neptune);
-      Pluto = THREEx.Planets.createPluto();
-      Pluto.geometry = new THREE.SphereGeometry(PlutoSize, 32, 32);
-      Pluto.position.set(0, 0, SunSize + (EarthBaseDistance * 30.44));
-      scene.add(Pluto);
-      /*
-      SaturnRing = new THREE.Mesh(new THREE.XRingGeometry(12, 20, 50, 6, 0, Math.PI * 2), new THREE.MeshBasicMaterial({
-        map: THREE.ImageUtils.loadTexture('lib/threex/threex.planets/images/saturn-rings.png'),
-        side: THREE.DoubleSide,
-        transparent: true,
-        opacity: 0.6
-      }));
-      scene.add(SaturnRing);
-      */
+      Sun = new THREE.Mesh();
+      Loader_Texture.load('img/planets/sunmap.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Sun = new THREE.Mesh(Geometry, Material);
+        scene.add(Sun);
+      });
+      Mercury = new THREE.Mesh();
+      Loader_Texture.load('img/planets/mercurymap.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Mercury = new THREE.Mesh(Geometry, Material);
+        Mercury.position.set(0, 0, 200);
+        scene.add(Mercury);
+      });
+      Venus = new THREE.Mesh();
+      Loader_Texture.load('img/planets/venusmap.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Venus = new THREE.Mesh(Geometry, Material);
+        Venus.position.set(0, 0, 400);
+        scene.add(Venus);
+      });
+      Earth = new THREE.Mesh();
+      Loader_Texture.load('img/planets/earthmap1k.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Earth = new THREE.Mesh(Geometry, Material);
+        Earth.position.set(0, 0, 600);
+        scene.add(Earth);
+      });
+      EarthMoon = new THREE.Mesh();
+      Loader_Texture.load('img/planets/moonmap1k.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        EarthMoon = new THREE.Mesh(Geometry, Material);
+        EarthMoon.position.set(200, 0, 600);
+        scene.add(EarthMoon);
+      });
+      Mars = new THREE.Mesh();
+      Loader_Texture.load('img/planets/marsmap1k.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Mars = new THREE.Mesh(Geometry, Material);
+        Mars.position.set(0, 0, 800);
+        scene.add(Mars);
+      });
+      Jupiter = new THREE.Mesh();
+      Loader_Texture.load('img/planets/jupitermap.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Jupiter = new THREE.Mesh(Geometry, Material);
+        Jupiter.position.set(0, 0, 1000);
+        scene.add(Jupiter);
+      });
+      Saturn = new THREE.Mesh();
+      Loader_Texture.load('img/planets/saturnmap.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Saturn = new THREE.Mesh(Geometry, Material);
+        Saturn.position.set(0, 0, 1200);
+        scene.add(Saturn);
+      });
+      SaturnRing = new THREE.Mesh();
+      Loader_Texture.load('img/planets/saturn-rings.png', function(texture) {
+        var Material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide, transparent: true, opacity: 0.6});
+        var Geometry = new THREE.XRingGeometry(100, 150, 50, 6, 0, Math.PI * 2);
+        SaturnRing = new THREE.Mesh(Geometry, Material);
+        SaturnRing.position.set(0, 0, 1200);
+        SaturnRing.rotation.x = -45 * (Math.PI / 180);
+        scene.add(SaturnRing);
+      });
+      Uranus = new THREE.Mesh();
+      Loader_Texture.load('img/planets/uranusmap.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Uranus = new THREE.Mesh(Geometry, Material);
+        Uranus.position.set(0, 0, 1400);
+        scene.add(Uranus);
+      });
+      UranusRing = new THREE.Mesh();
+      Loader_Texture.load('img/planets/saturn-rings.png', function(texture) {
+        var Material = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide, transparent: true, opacity: 0.6});
+        var Geometry = new THREE.XRingGeometry(100, 130, 50, 6, 0, Math.PI * 2);
+        UranusRing = new THREE.Mesh(Geometry, Material);
+        UranusRing.position.set(0, 0, 1400);
+        UranusRing.rotation.x = -80 * (Math.PI / 180);
+        scene.add(UranusRing);
+      });
+      Neptune = new THREE.Mesh();
+      Loader_Texture.load('img/planets/neptunemap.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Neptune = new THREE.Mesh(Geometry, Material);
+        Neptune.position.set(0, 0, 1600);
+        scene.add(Neptune);
+      });
+      Pluto = new THREE.Mesh();
+      Loader_Texture.load('img/planets/plutomap1k.jpg', function(texture) {
+        var Material = new THREE.MeshPhongMaterial({map: texture});
+        var Geometry = new THREE.SphereGeometry(100, 32, 32);
+        Pluto = new THREE.Mesh(Geometry, Material);
+        Pluto.position.set(0, 0, 1800);
+        scene.add(Pluto);
+      });
       clock = new THREE.Clock();
       animate();
     };
